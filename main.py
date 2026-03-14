5215c9c9aedd4eff257c29391041dd55ab64266c
dandancow874 <dandancow@gmail.com>
2026-03-15 03:30:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -944,8 +944,8 @@ class BigBanana(Star):
                 self.prompt_dict[cmd] = params
 
         fixed_prompts: dict[str, str] = {
-            "gp1": "gp1 {{user_text}} --min_images 0 --model grok-imagine-1.0",
-            "gp2": "gp2 {{user_text}} --min_images 1 --model grok-imagine-1.0-edit",
+            "gp1": "gp1 {{user_text}} --min_images 0 --model grok-imagine-1.0 --image_size 2K",
+            "gp2": "gp2 {{user_text}} --min_images 1 --model grok-imagine-1.0-edit --image_size 2K",
         }
         updated_prompts = False
         for trigger, prompt_line in fixed_prompts.items():
